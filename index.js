5a5a1a27bfbd0478bddbe25c2ac57c7063068fab
Miccer8 <michele.cere18@gmail.com>
2025-07-18 23:16:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-
 import express from 'express';
-import sweph from 'sweph';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
   res.send('Swiss Ephemeris microservice online.');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`✅ Server running on port ${port}`);
 });
